073fd121714c4029ba93ba5d61f792edd05e6078
Marco Martinez <mam002@ucsd.edu>
2011-11-16 09:44:18 -0800

    Added upgrade handlers for new scripts and style files

--- src/occams/form/upgrades/handlers/to_001.py
+++ src/occams/form/upgrades/handlers/to_001.py
@@ -1,8 +1,9 @@
+from Products.CMFCore.utils import getToolByName
 from occams.form import Logger as default_logger
 
+PROFILE_ID = u'profile-occams.form:default'
 
 def import_(context, logger=default_logger):
-    """
-    """
-    # TODO: jsregistry
-    # TODO: cssregistry
+    portal_setup = getToolByName(context, 'portal_setup')
+    portal_setup.runImportStepFromProfile(PROFILE_ID, 'cssregistry')
+    portal_setup.runImportStepFromProfile(PROFILE_ID, 'jsregistry')
