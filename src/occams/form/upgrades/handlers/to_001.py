f58fd42c0cf0a26c9ceb802c0e3c016860ef39fc
Marco Martinez <mam002@ucsd.edu>
2012-02-24 19:06:04 -0800

    Added new prodects as part of the upgrade step

--- src/occams/form/upgrades/handlers/to_001.py
+++ src/occams/form/upgrades/handlers/to_001.py
@@ -5,6 +5,8 @@ PROFILE_ID = u'profile-occams.form:default'
 
 def import_(context, logger=default_logger):
     portal_setup = getToolByName(context, 'portal_setup')
+    portal_setup.runAllImportStepsFromProfile('profile-plone.app.jquerytools:default')
+    portal_setup.runAllImportStepsFromProfile('profile-collective.z3cform.datagridfield:default')
     portal_setup.runImportStepFromProfile(PROFILE_ID, 'cssregistry')
     portal_setup.runImportStepFromProfile(PROFILE_ID, 'jsregistry')
     portal_setup.runImportStepFromProfile(PROFILE_ID, 'typeinfo')
