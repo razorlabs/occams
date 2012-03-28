73197a661f4de9b5a2bced013bfca5f06dbfc6d3
Jennifer Rodriguez-Mueller <jemueller@ucsd.edu>
2012-03-27 17:51:42 -0700

    Infrastructure for EAV move to avrc_data exists

---
+++ overhaul.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+"""Upgrade process for EAV versioning cleanup.
+
+#########1#########2#########3#########4#########5#########6#########7#####
+
+Precondition Assumptions:
+    1. There are two databases per run (eg avrc_demo_data and avrc_data)
+    2. The avrc_data will start as a straight up copy of avrc_demo_data.
+    3. For avrc_data we have "DROP TABLE"-ed: entity, state, datetime, 
+        integer, object, decimal, string, schema, attribute, and choice.
+    4. During the upgrade process, the website will be turned off.
+
+Design Assumptions:
+    1. Reflect avrc_demo_data w/ sqlsoup (readonly to ensure no changes).
+    2. No modifications to support tables (specimen, subject, etc) will be necessary.
+    3. Remove dates will respect chronology (ie data processed in order leaves final row with NULL remove date, and the rest "clean").
+    4. The process will run in one go, within a single night or weekend.
+
+"""
+from sqlalchemy.ext.sqlsoup import SqlSoup
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from occams.datastore import model
+
+from sqlalchemy import func
+from sqlalchemy import String
+from sqlalchemy import Date
+from sqlalchemy.sql.expression import case
+from sqlalchemy.sql.expression import cast
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import literal_column
+from sqlalchemy.orm import aliased
+
+global USER
+USER = (lambda : "bitcore@ucsd.edu") # can be called by a library
+
+global Session
+Session = scoped_session(sessionmaker())
+
+global old_model
+
+def main():
+    """Handle argv, specialize globals, launch job."""
+    import sys
+    usage = """overhaul.py OLDCONNECT NEWCONNECT"""
+    configureGlobalSession(sys.argv[1], sys.argv[2])
+    printListOfSchemas()
+    if isWorking():
+        print "Yay!"
+
+def moveIntoSchemaTable():
+    """Do general task over stuff produced by printListOfSchemas()"""
+    pass
+
+def printListOfSchemas(): 
+    global Session
+    changes = changeTableFactory(Session)
+    subforms = subSchemaNamesTableFactory(Session)
+    realChanges = (
+        Session.query(changes.c.schema_name, changes.c.change_date)
+        .filter(changes.c.change_date != None)
+        .filter(~changes.c.schema_name.in_(subforms))
+        .group_by(changes.c.schema_name, changes.c.change_date)
+        .order_by(changes.c.schema_name, changes.c.change_date)
+        )
+    #import pdb; pdb.set_trace()
+    print "\n".join(map(str,realChanges.all()))
+
+def configureGlobalSession(old_connect, new_connect):
+    """Set up Session for data manipulation and create new tables as side effect."""
+    global old_model
+    new_engine = create_engine(new_connect)
+    model.Model.metadata.create_all(bind=new_engine, checkfirst=True)
+    tables = []
+    tables_model = model.Model.metadata.sorted_tables
+    tables += dict.fromkeys(tables_model, new_engine).items()
+    #import pdb; pdb.set_trace()
+    Session.configure(binds=dict(tables))
+    old_model = SqlSoup(old_connect,session=Session)
+    #tables_datastore = old_model.sorted_tables
+    #tables += dict.fromkeys(tables_datastore, old_model.bind).items()
+
+#def makeSchema(blob):
+#    """Not used code yet... conceptual example from Marco"""
+#    schema = model.Schema(
+#       name='Foo',
+#       title='Foo Form',
+#       state='published',
+#       publish_date=date(),
+#       attributes=dict(
+#           model.Attribute(name='foo', title=u'Enter Foo', order=0, choices=[
+#               model.Choice(),
+#               model.Choice(),
+#           ]),
+#           model.Attribute(name='foo', title=u'Enter Foo', order=1),
+#           model.Attribute(name='foo', title=u'Enter Foo', order=2),
+#           model.Attribute(name='foo', title=u'Enter Foo', order=3),
+#       )
+#    Session.add(schema)
+#    Session.flush()
+#    assert schema.revision == 0
+#    schema.description = 'adfasdfadsfasd'
+#    Session.flush()
+#    assert schema.revision == 1
+#    newSchema = copy(schema)
+#    session.add(newSchema)
+
+def literal(value):
+    """Helper method to convert a Python value into a SQL string"""
+    return literal_column('\'%s\'' % str(value), String)
+
+def subSchemaNamesTableFactory(session):
+    """Helper method to generate a SQLAlchemy expression table for sub schemata names"""
+    query = (
+        session.query(old_model.entity("schema").name)
+        .join((old_model.entity("attribute"), (old_model.entity("attribute").object_schema_id == old_model.entity("schema").id)))
+        .group_by(old_model.entity("schema").name)
+        )
+    return query.subquery()
+
+def changeTableFactory(session):
+    """Helper method to generate a SQLAlchemy expression table for schemata changes.
+    The result set will include all modifications done to each schema."""
+    SubSchema = aliased(old_model.entity("schema"), name='_subschema')
+    SubAttribute = aliased(old_model.entity("attribute"), name='_subattribute')
+
+    # A query that builds a revision log result set of all master forms,
+    # which also include the revisions of subforms
+    query = (
+        # Master schema create dates
+        session.query(
+            old_model.entity("schema").name.label('schema_name'),
+            null().label('attribute_name'),
+            cast(old_model.entity("schema").create_date,Date).label("change_date"),
+            )
+        .union(
+            # Master schema removal dates
+            session.query(
+                old_model.entity("schema").name, 
+                null(), 
+                cast(old_model.entity("schema").remove_date,Date),
+                ),
+            # Master schema attribute create dates
+            session.query(
+                old_model.entity("schema").name,
+                # Only report non-object values as part of the field count
+                case([
+                    (old_model.entity("attribute").type != literal('object'),
+                        (old_model.entity("schema").name + literal('.') + old_model.entity("attribute").name))
+                    ]),
+                cast(old_model.entity("attribute").create_date,Date)
+                )
+            .join((old_model.entity("attribute"), (old_model.entity("attribute").schema_id == old_model.entity("schema").id))),
+
+            # Master schema attribute removal dates
+            session.query(
+                old_model.entity("schema").name, 
+                null(), 
+                cast(old_model.entity("attribute").remove_date,Date)
+                )
+            .join((old_model.entity("attribute"), (old_model.entity("attribute").schema_id == old_model.entity("schema").id))),
+
+            # Sub schema create dates
+            session.query(
+                old_model.entity("schema").name, 
+                null(),
+                cast(SubSchema.create_date,Date)
+                )
+            .join((old_model.entity("attribute"), (old_model.entity("attribute").schema_id == old_model.entity("schema").id)))
+            .join((SubSchema, (SubSchema.id == old_model.entity("attribute").object_schema_id))),
+
+            # Sub schema removal dates
+            session.query(
+                old_model.entity("schema").name, 
+                null(), 
+                cast(SubSchema.remove_date,Date)
+                )
+            .join((old_model.entity("attribute"), (old_model.entity("attribute").schema_id == old_model.entity("schema").id)))
+            .join((SubSchema, (SubSchema.id == old_model.entity("attribute").object_schema_id))),
+
+            # Sub schema attribute create dates
+            session.query(
+                old_model.entity("schema").name,
+                SubSchema.name + literal('.') + SubAttribute.name,
+                cast(SubAttribute.create_date,Date)
+                )
+            .join((old_model.entity("attribute"), (old_model.entity("attribute").schema_id == old_model.entity("schema").id)))
+            .join((SubSchema, (SubSchema.id == old_model.entity("attribute").object_schema_id)))
+            .join((SubAttribute, (SubAttribute.schema_id == SubSchema.id))),
+
+            # Sub schema attribute removal dates
+            session.query(
+                    old_model.entity("schema").name, 
+                    null(), 
+                    cast(SubAttribute.remove_date,Date)
+                    )
+            .join((old_model.entity("attribute"), (old_model.entity("attribute").schema_id == old_model.entity("schema").id)))
+            .join((SubSchema, (SubSchema.id == old_model.entity("attribute").object_schema_id)))
+            .join((SubAttribute, (SubAttribute.schema_id == SubSchema.id))),
+            )
+        )
+    return query.subquery('_change')
+
+def isWorking():
+    """Did [] work?"""
+    global old_model
+    good = True
+    if Session.query(old_model.entity("schema")).count() < 1:
+        print "SqlSoup broken"
+        good = False
+    if Session.query(model.Schema).count() > 0:
+        print "Session broken"
+        good = False
+    return good
+
+if __name__ == '__main__':
+    main()
+
