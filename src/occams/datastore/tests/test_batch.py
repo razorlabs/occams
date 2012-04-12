94a995657b8fd4d60ef47f46afd7444e8c36c260
Marco Martinez <mam002@ucsd.edu>
2012-04-11 17:40:40 -0700

    Batching unit tests completed with 100% coverage

--- src/occams/datastore/tests/test_batch.py
+++ src/occams/datastore/tests/test_batch.py
@@ -1,61 +1,118 @@
 
 import unittest2 as unittest
-
+import plone.testing
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.ext.declarative import declarative_base
 from zope.interface.verify import verifyClass
-
 from zope.interface.common.sequence import IFiniteSequence
-
 from z3c.batching.interfaces import IBatch
 
-from occams.datastore import model
 from occams.datastore.batch import SqlBatch
 from occams.datastore.batch import SqlBatches
 
-from occams.datastore.testing import DATASTORE_LAYER
+
+Base = declarative_base()
+
+
+class Sample(Base):
+    __tablename__ = 'sample'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+    def objectify(self):
+        """
+        Make sure the legacy crap still works...
+        """
+        return self
 
 
 class BatchingTestCase(unittest.TestCase):
     """
-    Verifies DataStore Entity storage
+    Verifies proper sqlalchemy batching
     """
 
-    layer = DATASTORE_LAYER
+    class BatchingLayer(plone.testing.Layer):
+        """
+        Local layer for testing made-up mappers
+        """
+
+        def setUp(self):
+            engine = create_engine('sqlite://')
+            self['session'] = scoped_session(sessionmaker(bind=engine))
+            Base.metadata.create_all(engine)
+
+        def testTearDown(self):
+            self['session'].rollback()
+
+    layer = BatchingLayer()
 
     def setUp(self):
-        self.session = self.layer['session']
-        self.session.add_all([
-            model.Schema(name='Bar', title=u'Doesn\'t matter'), # 0
-            model.Schema(name='Baz', title=u'Doesn\'t matter'), # 1
-            model.Schema(name='Caz', title=u'Doesn\'t matter'), # 2
-            model.Schema(name='Foo', title=u'Doesn\'t matter'), # 3
-            model.Schema(name='Jaz', title=u'Doesn\'t matter'), # 4
-            model.Schema(name='Raz', title=u'Doesn\'t matter'), # 5
-            ])
-        self.session.flush()
+        session = self.layer['session']
 
-    def test_implementation(self):
+        # Sample test data so that we don't have to keep recreating it
+        session.add_all([
+            Sample(name='Bar'), # 0
+            Sample(name='Baz'), # 1
+            Sample(name='Caz'), # 2
+            Sample(name='Foo'), # 3
+            Sample(name='Jaz'), # 4
+            Sample(name='Raz'), # 5
+            ])
+        session.flush()
+
+    def testImplementation(self):
         self.assertTrue(verifyClass(IBatch, SqlBatch))
         self.assertTrue(verifyClass(IFiniteSequence, SqlBatches))
 
-    def test_first_element(self):
-        session = self.session
-        query = session.query(model.Schema).order_by(model.Schema.name)
+    def testInit(self):
+        session = self.layer['session']
+        # Validate proper starting point with empty sequence
+        query = session.query(Sample).limit(0)
+        batch = SqlBatch(query)
+        self.assertEqual(-1, batch.start)
+        self.assertEqual(-1, batch.end)
+
+        # Validate improper starting point past the length
+        query = session.query(Sample)
+        with self.assertRaises(IndexError):
+            batch = SqlBatch(query, start=10)
+
+        # Validate correct "true size" (the number of remaining items in the batch)
+        query = session.query(Sample)
+        batch = SqlBatch(query, start=0, size=3)
+        self.assertEqual(3, batch._trueSize)
+        self.assertEqual(2, batch.end)
+
+        # Since the batch is greater than the total number of items, it will
+        # just return the number of remaining
+        batch = SqlBatch(query, start=0, size=10)
+        self.assertEqual(6, batch._trueSize)
+        self.assertEqual(5, batch.end)
+
+    def testFirstElement(self):
+        session = self.layer['session']
+        query = session.query(Sample).order_by(Sample.name)
         batch = SqlBatch(query)
         (id, item) = batch.firstElement
         self.assertIsNotNone(id)
-        self.assertEqual('Bar', item.name)
+        self.assertEqual(item.name, 'Bar')
 
-    def test_last_element(self):
-        session = self.session
-        query = session.query(model.Schema).order_by(model.Schema.name)
+    def testLastElement(self):
+        session = self.layer['session']
+        query = session.query(Sample).order_by(Sample.name)
         batch = SqlBatch(query)
         (id, item) = batch.lastElement
         self.assertIsNotNone(id)
         self.assertEqual(item.name, 'Raz')
 
-    def test_getitem(self):
-        session = self.session
-        query = session.query(model.Schema).order_by(model.Schema.name)
+    def testGetItem(self):
+        session = self.layer['session']
+        query = session.query(Sample).order_by(Sample.name)
         batch = SqlBatch(query)
         (id, item) = batch[3]
         self.assertIsNotNone(id)
@@ -63,40 +120,62 @@ class BatchingTestCase(unittest.TestCase):
         with self.assertRaises(IndexError):
             batch[10]
 
-    def test_len(self):
-        session = self.session
-        query = session.query(model.Schema).order_by(model.Schema.name)
+    def testLen(self):
+        session = self.layer['session']
+        query = session.query(Sample).order_by(Sample.name)
         batch = SqlBatch(query)
         self.assertEqual(len(batch), 6)
 
-    def test_contains(self):
-        session = self.session
-        query = session.query(model.Schema).order_by(model.Schema.name)
+    def testContains(self):
+        session = self.layer['session']
+        query = session.query(Sample).order_by(Sample.name)
         batch = SqlBatch(query)
-        item = session.query(model.Schema).filter_by(name='Bar').one()
+        item = self.layer['session'].query(Sample).filter_by(name='Bar').one()
         self.assertIn(item, batch)
 
-        item = model.Schema(id=99, name='Blarg', title=u'Blah')
+        item = Sample(id=99, name='Blarg')
         self.assertNotIn(item, batch)
 
-    def test_getslice_(self):
-        session = self.session
-        query = session.query(model.Schema).order_by(model.Schema.name)
+    def testGetSlice(self):
+        session = self.layer['session']
+        query = session.query(Sample).order_by(Sample.name)
         batch = SqlBatch(query)
         generator = batch[2:4]
         self.assertIsNotNone(generator)
-        names = tuple([r.name for i, r in generator])
-        self.assertListEqual(['Caz', 'Foo'], list(names))
+        names = [r.name for i, r in generator]
+        self.assertListEqual(['Caz', 'Foo'], names)
 
-    def test_eq(self):
-        session = self.session
-        query = session.query(model.Schema)
+        # Should anchor to the "tru size"
+        generator = batch[4:200]
+        names = [r.name for i, r in generator]
+        self.assertListEqual(['Jaz', 'Raz'], names)
+
+    def testEquals(self):
+        session = self.layer['session']
+        query = session.query(Sample)
         batch = SqlBatch(query)
         other = SqlBatch(query)
         # Works with batches with the same query instance
         self.assertEqual(batch, other)
-        other = SqlBatch(session.query(model.Schema))
+        other = SqlBatch(session.query(Sample))
         # Doesn't work with same query, yet different query object
         self.assertNotEqual(batch, other)
         other = SqlBatch(query, size=1000)
         self.assertNotEqual(batch, other)
+
+    def testBatches(self):
+        session = self.layer['session']
+        query = session.query(Sample)
+        # Will generate three "pages" of two items each
+        batch = SqlBatch(query, size=2)
+        page1 = batch.batches[0]
+        self.assertEqual(batch, page1)
+
+        page2 = batch.batches[1]
+        self.assertNotEqual(batch, page2)
+        self.assertListEqual(['Caz', 'Foo'], [r.name for i, r in page2])
+
+        # Not go backwards, by getting the last page
+        last = batch.batches[-1]
+        self.assertNotEqual(batch, last)
+        self.assertListEqual(['Jaz', 'Raz'], [r.name for i, r in last])
