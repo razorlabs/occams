c6ca377266de084975c742ff0d44b547c5cc642d
Marco Martinez <mam002@ucsd.edu>
2012-04-24 12:46:08 -0700

    Reached a stable point in querying module

--- src/occams/datastore/tests/test_query.py
+++ src/occams/datastore/tests/test_query.py
@@ -7,8 +7,10 @@ from datetime import date
 
 from occams.datastore.testing import DATASTORE_LAYER
 from occams.datastore import model
+from occams.datastore.interfaces import InvalidEntitySchemaError
+from occams.datastore.query import Split
 from occams.datastore.query import schemaToSubQuery
-from occams.datastore.query import getHeader
+from occams.datastore.query import getAttributes
 from occams.datastore.schema import copy
 
 
@@ -25,7 +27,7 @@ class QueryTestCase(unittest.TestCase):
 
     layer = DATASTORE_LAYER
 
-    def testGetHeader(self):
+    def testGetAttributes(self):
         session = self.layer['session']
         schema1 = model.Schema(
             name='A',
@@ -38,11 +40,11 @@ class QueryTestCase(unittest.TestCase):
             )
         session.add(schema1)
 
-        names, columns = getHeader(session, 'A', split=False)
+        names, columns = getAttributes(session, 'A', split=Split.NAME)
         self.assertItemsEqual(['a'], names)
         self.assertItemsEqual(['a'], columns.keys())
 
-        names, columns = getHeader(session, 'A', split=True)
+        names, columns = getAttributes(session, 'A', split=Split.CHECKSUM)
         expected = ['a_' + schema1['a'].checksum]
         self.assertItemsEqual(expected, names)
         self.assertItemsEqual(expected, columns.keys())
@@ -58,60 +60,129 @@ class QueryTestCase(unittest.TestCase):
             )
         session.add(schema2)
 
-        names, columns = getHeader(session, 'A', split=False)
+        names, columns = getAttributes(session, 'A', split=Split.NAME)
         self.assertItemsEqual(['a'], names)
         self.assertItemsEqual(['a'], columns.keys())
 
-        names, columns = getHeader(session, 'A', split=True)
+        names, columns = getAttributes(session, 'A', split=Split.CHECKSUM)
         expected = ['a_' + schema1['a'].checksum, 'a_' + schema2['a'].checksum]
         self.assertItemsEqual(expected, names)
         self.assertItemsEqual(expected, columns.keys())
 
-#    def testUnpublished(self):
-#        session = self.layer['session']
-#        schema = model.Schema(name='A', title=u'', state='draft')
-#        session.add(schema)
-#        session.flush()
-#
-##        subquery = schemaToSubQuery(session, 'A', split=True)
-#
-#    def testEmpty(self):
-#        session = self.layer['session']
-#        schema = model.Schema(name='A', title=u'', state='published', publish_date=p1)
-#        session.add(schema)
-#        session.flush()
-#
-#        subquery = schemaToSubQuery(session, 'A', split=True)
-#
-#    def testFlatString(self):
-#        session = self.layer['session']
-#        schema = model.Schema(
-#            name='A',
-#            title=u'',
-#            state='published',
-#            publish_date=p1,
-#            attributes=dict(
-#                a=model.Attribute(name='a', title=u'', type='string', order=0),
-#                )
-#            )
-#        session.add(schema)
-#        session.flush()
-#
-#        entity = model.Entity(schema=schema, name='Foo', title=u'')
-#        session.add(entity)
-#        entity['a'] = u'foo'
-#        session.flush()
-#
-#        entity = model.Entity(schema=schema, name='Bar', title=u'')
-#        session.add(entity)
-#        entity['a'] = u'Bar'
-#        session.flush()
-#
-#        subquery = schemaToSubQuery(session, 'A', split=True)
-#        for s in session.query(subquery):
-#            print s
-#
-#    def testSubSchemata(self):
-#        pass
+    def testGetAttributesWithSubSchema(self):
+        session = self.layer['session']
+        schema1 = model.Schema(
+            name='Main',
+            title=u'',
+            state='published',
+            publish_date=p1,
+            attributes=dict(
+                a=model.Attribute(name='a', title=u'', type='string', order=0),
+                sub=model.Attribute(
+                    name='sub',
+                    title=u'',
+                    type='object',
+                    order=1,
+                    object_schema=model.Schema(
+                        name='Sub',
+                        title=u'',
+                        state='published',
+                        publish_date=p1,
+                        attributes=dict(
+                            x=model.Attribute(name='x', title=u'', type='string', order=0),
+                            )
+                        )
+                    )
+                )
+            )
+        session.add(schema1)
 
+        names, columns = getAttributes(session, 'Main', split=Split.NAME)
+        expected = ['a', 'sub_x']
+        self.assertItemsEqual(expected, names)
+        self.assertItemsEqual(expected, columns.keys())
 
+        names, columns = getAttributes(session, 'Main', split=Split.CHECKSUM)
+        expected = ['a_' + schema1['a'].checksum, 'sub_x_' + schema1['sub']['x'].checksum]
+        self.assertItemsEqual(expected, names)
+        self.assertItemsEqual(expected, columns.keys())
+
+        schema2 = copy(schema1)
+        schema2.state = u'published'
+        schema2.publish_date = p2
+        schema2['sub'].object_schema.state = u'published'
+        schema2['sub'].object_schema.publish_date = p2
+        schema2['sub']['x'].title = u'New title'
+        session.add(schema2)
+        session.flush()
+
+        names, columns = getAttributes(session, 'Main', split=Split.NAME)
+        expected = ['a', 'sub_x']
+        self.assertItemsEqual(expected, names)
+        self.assertItemsEqual(expected, columns.keys())
+
+        names, columns = getAttributes(session, 'Main', split=Split.CHECKSUM)
+        expected = [
+            'a_' + schema1['a'].checksum,
+            'sub_x_' + schema1['sub']['x'].checksum,
+            'sub_x_' + schema2['sub']['x'].checksum,
+            ]
+
+        self.assertItemsEqual(expected, names)
+        self.assertItemsEqual(expected, columns.keys())
+
+    def testUnpublishedSchema(self):
+        session = self.layer['session']
+        schema = model.Schema(name='A', title=u'', state='draft')
+        session.add(schema)
+        session.flush()
+
+        with self.assertRaises(InvalidEntitySchemaError):
+            subquery = schemaToSubQuery(session, 'A', split=False)
+
+    def testEmptySchema(self):
+        session = self.layer['session']
+        schema = model.Schema(name='A', title=u'', state='published', publish_date=p1)
+        session.add(schema)
+        session.flush()
+
+        subquery = schemaToSubQuery(session, 'A', split=True)
+
+        self.assertEqual(0, session.query(subquery).count())
+
+        entity = model.Entity(schema=schema, name='Sample', title=u'')
+        session.add(entity)
+        session.flush()
+
+        self.assertEqual(1, session.query(subquery).count())
+
+    def testFlatSchemaWithString(self):
+        session = self.layer['session']
+        schema = model.Schema(
+            name='A',
+            title=u'',
+            state='published',
+            publish_date=p1,
+            attributes=dict(
+                a=model.Attribute(name='a', title=u'', type='string', order=0),
+                )
+            )
+        session.add(schema)
+        session.flush()
+
+        entity = model.Entity(schema=schema, name='Foo', title=u'')
+        session.add(entity)
+        entity['a'] = u'foo'
+        session.flush()
+
+        entity = model.Entity(schema=schema, name='Bar', title=u'')
+        session.add(entity)
+        entity['a'] = u'Bar'
+        session.flush()
+
+        subquery = schemaToSubQuery(session, 'A', split=True)
+        for s in session.query(subquery):
+            print s
+
+    def testSubSchemataWithString(self):
+        pass
