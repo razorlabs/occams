de133e40373a97ade6f7d0f8d5443fd68eebde0f
Marco Martinez <mam002@ucsd.edu>
2012-04-30 21:41:34 -0700

    Column plan method implemented

--- src/occams/datastore/query.py
+++ src/occams/datastore/query.py
@@ -30,27 +30,50 @@ class Split(object):
     (NAME, CHECKSUM, ID) = range(3)
 
 
-def schemaToSubQuery(session, name, split=Split.NAME):
+def defaultColumnNaming(session, name, split, path, attributes):
+    """
+    Implements default naming behavior of subquery columns
+    """
+    return '_'.join(path)
+
+
+def schemaToSubquery(session, name, split=Split.NAME, merge=dict(), naming=defaultColumnNaming):
     """
     Returns a schema entity data as an aliased sub-query.
 
     Arguments
+        ``session``
+            The session to query attributes from
         ``name``
             The name of the schema family to generate the subquery for
         ``split``
-            Optional parameter to split aggressively split the variable
-            names by checksum. False by default.
-
+            (Optional) the splitting algorithm to use. Default is by name.
+        ``merge``
+            (Optional) a reverse lookup dictionary to how columns should be combined
+            Essentially, a dictionary who's key / value pairs are interpreted
+            as: "column_name" / "column_to_combine_with"
+        ``naming``
+            (Optional) The naming scheme to use, default is ``defaultColumnNaming``
+            Client applications can override this by passing a method
+            callback which will be passed the following parameters:
+                * session: session being used
+                * name: the schema that the subquery is being generated for
+                * split: splitting algorithm used
+                * path: the key in the column plan
+                * attributes: the attributes for the column
+            Note that merge is not passed as a parameter as this method
+            takes care of the final column merging.
     Returns
         A subquery representation of the schema family.
+
         Note that the results that will be retured by the subquery are
-        namedtuples of each result
+        named tuples of each result using the names of the naming
+        schema as the property names.
 
     """
 
     # Collapse all the related attributes into an ordered tree
-    columns = getAttributes(session, name, split=split)
-    names = columns.keys()
+    plan = getColumnPlan(session, name, split=split)
 
     # TODO somewhere in here we further collapse the attributes based on a
     # configuration file
@@ -62,12 +85,14 @@ def schemaToSubQuery(session, name, split=Split.NAME):
             model.Entity.collect_date.label('entity_collect_date'),
             )
         .join(model.Entity.schema)
-        .filter(model.Schema.name == name)
-        .filter(model.Schema.publish_date != None)
+        .filter((model.Schema.name == name) & (model.Schema.publish_date != None))
         )
 
-    for name in names:
-        for attribute in columns[name]:
+    for path, attributes in plan.iteritems():
+        for attribute in attributes:
+            columnName = defaultColumnNaming(session, name, split, path, attributes)
+
+            # YOU LEFT OFF HERE
 
             Value = aliased(nameModelMap[attribute.type])
 
@@ -89,25 +114,42 @@ def schemaToSubQuery(session, name, split=Split.NAME):
 
                 column = cast(Value._value, nameCastMap[attribute.type])
 
-            exportQuery = exportQuery.add_column(column.label(name))
+            exportQuery = exportQuery.add_column(column.label(columnName))
 
     subQuery = exportQuery.subquery(name)
     return subQuery
 
 
-def getAttributes(session, name, split=Split.NAME, prefix=''):
+def getColumnPlan(session, name, split=Split.NAME):
     """
-    Consolidates all the attributes in a schema hierarchy into a single listing.
+    Consolidates all the plan in a schema hierarchy into a single listing.
     The way this is accomplished is by traversing all the nodes in each
     hierarchy level and reporting only the leaf nodes (i.e. basic data types,
     not sub schemata)
+
+    Note that the final columns are ordered by most recent order number within
+    the parent, then by the parent's publication date (oldest to newest).
+
+    Arguments
+        ``session``
+            The session to query plan from
+        ``name``
+            The name of the schema to get columns plans for
+        ``split``
+            (Optional) the splitting algorithm to use. Default is by name.
+
+    Returns
+        An ordered dictionary using the path to the attribute as the key,
+        and the associated attribute list as the value. Depending on
+        the splitting method specified, the path will also have
+        the attributes' id or checksum as well.
+
     """
-    attributes = OrderedDict()
+    plan = OrderedDict()
 
     schemaCheckQuery = (
         session.query(model.Schema)
-        .filter(model.Schema.name == name)
-        .filter(model.Schema.publish_date != None)
+        .filter((model.Schema.name == name) & (model.Schema.publish_date != None))
         )
 
     count = schemaCheckQuery.count()
@@ -115,39 +157,47 @@ def getAttributes(session, name, split=Split.NAME, prefix=''):
     if count <= 0:
         raise InvalidEntitySchemaError(name)
 
+    # Aliased so we don't get naming ambiguity
+    RecentAttribute = aliased(model.Attribute, name='recent_attribute')
+
+    # Build a subquery that determines an attribute's most recent order
+    recentOrderSubQuery = (
+        session.query(RecentAttribute.order)
+        .join(RecentAttribute.schema)
+        .filter((model.Schema.name == name) & (model.Schema.publish_date != None))
+        .filter(RecentAttribute.name == model.Attribute.name)
+        .order_by(model.Schema.publish_date.desc())
+        .limit(1)
+        .correlate(model.Attribute)
+        .as_scalar()
+        )
+
+    # Get all the attributes ordered by their most recent order, then oldest to newest
     attributeQuery = (
         session.query(model.Attribute)
         .join(model.Attribute.schema)
-        .filter(model.Schema.name == name)
-        .filter(model.Schema.publish_date != None)
+        .filter((model.Schema.name == name) & (model.Schema.publish_date != None))
         .order_by(
+            recentOrderSubQuery.asc(),
             model.Schema.publish_date.asc(),
-            model.Attribute.order.asc(),
             )
         )
 
     for attribute in attributeQuery:
         if attribute.type == 'object':
-            subattributes = getAttributes(
-                session=session,
-                name=attribute.object_schema.name,
-                prefix=attribute.name + '_',
-                split=split,
-                )
-
-            attributes.update(subattributes)
+            subattributes = getColumnPlan(session, attribute.object_schema.name, split)
+            for path, items in subattributes.iteritems():
+                plan[(attribute.name,) + path] = items
         else:
-            specialName = prefix + attribute.name
-
             if split == Split.CHECKSUM:
-                specialName += '_' + attribute.checksum
+                path = (attribute.name, attribute.checksum)
+            elif split == Split.ID:
+                path = (attribute.name, attribute.id)
+            else:
+                path = (attribute.name,)
+            plan.setdefault(path, []).append(attribute)
 
-            if specialName not in attributes:
-                attributes.setdefault(specialName, [])
-
-            attributes[specialName].append(attribute)
-
-    return attributes
+    return plan
 
 
 class collection(ColumnElement):
--- src/occams/datastore/tests/test_query.py
+++ src/occams/datastore/tests/test_query.py
@@ -9,8 +9,8 @@ from occams.datastore.testing import OCCAMS_DATASTORE_MODEL_FIXTURE
 from occams.datastore import model
 from occams.datastore.interfaces import InvalidEntitySchemaError
 from occams.datastore.query import Split
-from occams.datastore.query import schemaToSubQuery
-from occams.datastore.query import getAttributes
+from occams.datastore.query import schemaToSubquery
+from occams.datastore.query import getColumnPlan
 from occams.datastore.schema import copy
 
 
@@ -20,117 +20,292 @@ p3 = date(2011, 8, 1)
 p4 = date(2012, 5, 1)
 
 
-class QueryTestCase(unittest.TestCase):
+class ColumnPlanTestCase(unittest.TestCase):
     """
-    Verifies the subquery adapter
+    Verifies column header
     """
 
     layer = OCCAMS_DATASTORE_MODEL_FIXTURE
 
-    def testGetAttributes(self):
-        session = self.layer['session']
-        schema1 = model.Schema(
-            name='A',
-            title=u'',
-            state='published',
-            publish_date=p1,
-            attributes=dict(
-                a=model.Attribute(name='a', title=u'', type='string', order=0),
-                )
-            )
-        session.add(schema1)
-
-        columns = getAttributes(session, 'A', split=Split.NAME)
-        self.assertItemsEqual(['a'], columns.keys())
-
-        columns = getAttributes(session, 'A', split=Split.CHECKSUM)
-        expected = ['a_' + schema1['a'].checksum]
-        self.assertItemsEqual(expected, columns.keys())
-
-        schema2 = model.Schema(
-            name='A',
-            title=u'',
-            state='published',
-            publish_date=p2,
-            attributes=dict(
-                a=model.Attribute(name='a', title=u'New title', type='string', order=0),
-                )
-            )
-        session.add(schema2)
-
-        columns = getAttributes(session, 'A', split=Split.NAME)
-        self.assertItemsEqual(['a'], columns.keys())
-
-        columns = getAttributes(session, 'A', split=Split.CHECKSUM)
-        expected = ['a_' + schema1['a'].checksum, 'a_' + schema2['a'].checksum]
-        self.assertItemsEqual(expected, columns.keys())
-
-    def testGetAttributesWithSubSchema(self):
-        session = self.layer['session']
-        schema1 = model.Schema(
-            name='Main',
-            title=u'',
-            state='published',
-            publish_date=p1,
-            attributes=dict(
-                a=model.Attribute(name='a', title=u'', type='string', order=0),
-                sub=model.Attribute(
-                    name='sub',
-                    title=u'',
-                    type='object',
-                    order=1,
-                    object_schema=model.Schema(
-                        name='Sub',
-                        title=u'',
-                        state='published',
-                        publish_date=p1,
-                        attributes=dict(
-                            x=model.Attribute(name='x', title=u'', type='string', order=0),
-                            )
-                        )
-                    )
-                )
-            )
-        session.add(schema1)
-
-        columns = getAttributes(session, 'Main', split=Split.NAME)
-        expected = ['a', 'sub_x']
-        self.assertItemsEqual(expected, columns.keys())
-
-        columns = getAttributes(session, 'Main', split=Split.CHECKSUM)
-        expected = ['a_' + schema1['a'].checksum, 'sub_x_' + schema1['sub']['x'].checksum]
-        self.assertItemsEqual(expected, columns.keys())
-
-        schema2 = copy(schema1)
-        schema2.state = u'published'
-        schema2.publish_date = p2
-        schema2['sub'].object_schema.state = u'published'
-        schema2['sub'].object_schema.publish_date = p2
-        schema2['sub']['x'].title = u'New title'
-        session.add(schema2)
-        session.flush()
-
-        columns = getAttributes(session, 'Main', split=Split.NAME)
-        expected = ['a', 'sub_x']
-        self.assertItemsEqual(expected, columns.keys())
-
-        columns = getAttributes(session, 'Main', split=Split.CHECKSUM)
-        expected = [
-            'a_' + schema1['a'].checksum,
-            'sub_x_' + schema1['sub']['x'].checksum,
-            'sub_x_' + schema2['sub']['x'].checksum,
-            ]
-
-        self.assertItemsEqual(expected, columns.keys())
-
     def testUnpublishedSchema(self):
         session = self.layer['session']
-        schema = model.Schema(name='A', title=u'', state='draft')
-        session.add(schema)
+        session.add(model.Schema(name='A', title=u'', state='draft'))
         session.flush()
 
         with self.assertRaises(InvalidEntitySchemaError):
-            subquery = schemaToSubQuery(session, 'A', split=False)
+            plan = getColumnPlan(session, 'A')
+
+    def testEmptyPublishedSchema(self):
+        session = self.layer['session']
+        session.add(model.Schema(name='A', title=u'', state='published'))
+        session.flush()
+        plan = getColumnPlan(session, 'A')
+        self.assertEqual(0, len(plan))
+
+    def testKeysFromSingleForm(self):
+        session = self.layer['session']
+        schema = model.Schema(name='A', title=u'', state='published',
+            attributes=dict(
+                a=model.Attribute(name='a', title=u'', type='string', order=0),
+                )
+            )
+        session.add(schema)
+        session.flush()
+
+        # By Name
+        plan = getColumnPlan(session, 'A', split=Split.NAME)
+        expected = [('a',)]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(1, len(plan[expected[0]]))
+
+        # By Checksum
+        plan = getColumnPlan(session, 'A', split=Split.CHECKSUM)
+        expected = [('a', schema['a'].checksum)]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(1, len(plan[expected[0]]))
+
+        # By id
+        plan = getColumnPlan(session, 'A', split=Split.ID)
+        expected = [('a', schema['a'].id)]
+        self.assertListEqual(expected, plan.keys())
+        for e in expected:
+            self.assertEqual(1, len(plan[e]))
+
+    def testKeysFromMultpleVersions(self):
+        session = self.layer['session']
+        schema1 = model.Schema(name='A', title=u'', state='published', publish_date=p1,
+            attributes=dict(
+                a=model.Attribute(name='a', title=u'', type='string', order=0),
+                )
+            )
+
+        schema2 = copy(schema1)
+        schema2.state = 'published'
+        schema2.publish_date = p2
+
+        schema3 = copy(schema1)
+        schema3.state = 'published'
+        schema3.publish_date = p3
+        schema3['a'].title = u'prime'
+
+        session.add_all([schema1, schema2, schema3])
+
+        # By Name
+        plan = getColumnPlan(session, 'A', split=Split.NAME)
+        expected = [('a',)]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(3, len(plan[expected[0]]))
+
+        # By Checksum
+        plan = getColumnPlan(session, 'A', split=Split.CHECKSUM)
+        expected = [('a', schema1['a'].checksum), ('a', schema3['a'].checksum)]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(2, len(plan[expected[0]]))
+        self.assertEqual(1, len(plan[expected[1]]))
+
+        # By id
+        plan = getColumnPlan(session, 'A', split=Split.ID)
+        expected = [
+            ('a', schema1['a'].id),
+            ('a', schema2['a'].id),
+            ('a', schema3['a'].id)
+            ]
+        self.assertListEqual(expected, plan.keys())
+        for e in expected:
+            self.assertEqual(1, len(plan[e]))
+
+    def testKeysFromSubSchema(self):
+        session = self.layer['session']
+        schema = model.Schema(name='A', title=u'', state='published',
+            attributes=dict(
+                a=model.Attribute(name='a', title=u'', type='string', order=0),
+                b=model.Attribute(name='b', title=u'', type='object', order=1,
+                    object_schema=model.Schema(name='B', title=u'', state='published',
+                        attributes=dict(
+                            x=model.Attribute(name='x', title=u'', type='string', order=0,),
+                            y=model.Attribute(name='y', title=u'', type='string', order=1,),
+                            z=model.Attribute(name='z', title=u'', type='string', order=2,),
+                            )
+                        )
+                    ),
+                c=model.Attribute(name='c', title=u'', type='string', order=2),
+                )
+            )
+        session.add(schema)
+        session.flush()
+
+        # By Name
+        plan = getColumnPlan(session, 'A', split=Split.NAME)
+        expected = [('a',), ('b', 'x'), ('b', 'y'), ('b', 'z'), ('c',)]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(1, len(plan[expected[0]]))
+        self.assertEqual(1, len(plan[expected[1]]))
+        self.assertEqual(1, len(plan[expected[2]]))
+        self.assertEqual(1, len(plan[expected[3]]))
+        self.assertEqual(1, len(plan[expected[4]]))
+
+        # By Checksum
+        plan = getColumnPlan(session, 'A', split=Split.CHECKSUM)
+        expected = [
+            ('a', schema['a'].checksum),
+            ('b', 'x', schema['b']['x'].checksum),
+            ('b', 'y', schema['b']['y'].checksum),
+            ('b', 'z', schema['b']['z'].checksum),
+            ('c', schema['c'].checksum),
+            ]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(1, len(plan[expected[0]]))
+        self.assertEqual(1, len(plan[expected[1]]))
+        self.assertEqual(1, len(plan[expected[2]]))
+        self.assertEqual(1, len(plan[expected[3]]))
+        self.assertEqual(1, len(plan[expected[4]]))
+
+        # By id
+        plan = getColumnPlan(session, 'A', split=Split.ID)
+        expected = [
+            ('a', schema['a'].id),
+            ('b', 'x', schema['b']['x'].id),
+            ('b', 'y', schema['b']['y'].id),
+            ('b', 'z', schema['b']['z'].id),
+            ('c', schema['c'].id),
+            ]
+        self.assertListEqual(expected, plan.keys())
+        for e in expected:
+            self.assertEqual(1, len(plan[e]))
+
+    def testKeysFromSubSchemaMultipleVersions(self):
+        """
+        From the discussion Jennifer and I had:
+        Time p1:
+                A
+              / | \
+             a  b  c
+              / | \
+             x  y  z
+
+        Time p2:
+            ``a`` is modifiied
+            ``y`` is modified
+            ``b`` children are reordered (doesn't affect checksums, but
+                considered on output
+
+                A
+              / | \
+             a' b  c
+              / | \
+             z  x  y'
+
+        Time p3:
+            ``a`` is restored
+            ``x`` is removed
+            ``y`` is removed
+            ``y*`` is added to the parent schema (interpreted as new attribute
+                because it belongs to a new partent)
+
+                A
+              / | \
+             a  b  y*
+                |
+                z
+        """
+        session = self.layer['session']
+        schema1 = model.Schema(name='A', title=u'', state='published', publish_date=p1,
+            attributes=dict(
+                a=model.Attribute(name='a', title=u'', type='string', order=0),
+                b=model.Attribute(name='b', title=u'', type='object', order=1,
+                    object_schema=model.Schema(name='B', title=u'', state='published', publish_date=p1,
+                        attributes=dict(
+                            x=model.Attribute(name='x', title=u'', type='string', order=0,),
+                            y=model.Attribute(name='y', title=u'', type='string', order=1,),
+                            z=model.Attribute(name='z', title=u'', type='string', order=2,),
+                            )
+                        )
+                    ),
+                c=model.Attribute(name='c', title=u'', type='string', order=2),
+                )
+            )
+
+        schema2 = copy(schema1)
+        schema2.state = schema2['b'].object_schema.state = 'published'
+        schema2.publish_date = schema2['b'].object_schema.publish_date = p2
+        schema2['a'].title = u'prime'
+        schema2['b']['y'].title = u'prime'
+        schema2['b']['z'].order = 0
+        schema2['b']['x'].order = 1
+        schema2['b']['y'].order = 2
+
+        schema3 = copy(schema2)
+        schema3.state = schema3['b'].object_schema.state = 'published'
+        schema3.publish_date = schema3['b'].object_schema.publish_date = p3
+        schema3['a'] = model.Attribute(name='a', title=u'', type='string', order=0)
+        del schema3['b']['x']
+        del schema3['b']['y']
+        del schema3['c']
+        schema3['y'] = model.Attribute(name='y', title=u'', type='string', order=2,)
+
+        session.add_all([schema1, schema2, schema3])
+        session.flush()
+
+        # By Name
+        plan = getColumnPlan(session, 'A', split=Split.NAME)
+        expected = [('a',), ('b', 'z'), ('b', 'x'), ('b', 'y'), ('c',), ('y',)]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(3, len(plan[expected[0]]))
+        self.assertEqual(3, len(plan[expected[1]]))
+        self.assertEqual(2, len(plan[expected[2]]))
+        self.assertEqual(2, len(plan[expected[3]]))
+        self.assertEqual(2, len(plan[expected[4]]))
+        self.assertEqual(1, len(plan[expected[5]]))
+
+        # By Checksum
+        plan = getColumnPlan(session, 'A', split=Split.CHECKSUM)
+        expected = [
+            ('a', schema1['a'].checksum),
+            ('a', schema2['a'].checksum),
+            ('b', 'z', schema1['b']['z'].checksum),
+            ('b', 'x', schema1['b']['x'].checksum),
+            ('b', 'y', schema1['b']['y'].checksum),
+            ('b', 'y', schema2['b']['y'].checksum),
+            ('c', schema1['c'].checksum),
+            ('y', schema3['y'].checksum),
+            ]
+        self.assertListEqual(expected, plan.keys())
+        self.assertEqual(2, len(plan[expected[0]]))
+        self.assertEqual(1, len(plan[expected[1]]))
+        self.assertEqual(3, len(plan[expected[2]]))
+        self.assertEqual(2, len(plan[expected[3]]))
+        self.assertEqual(1, len(plan[expected[4]]))
+        self.assertEqual(1, len(plan[expected[5]]))
+        self.assertEqual(2, len(plan[expected[6]]))
+        self.assertEqual(1, len(plan[expected[7]]))
+
+        # By id
+        plan = getColumnPlan(session, 'A', split=Split.ID)
+        expected = [
+            ('a', schema1['a'].id),
+            ('a', schema2['a'].id),
+            ('a', schema3['a'].id),
+            ('b', 'z', schema1['b']['z'].id),
+            ('b', 'z', schema2['b']['z'].id),
+            ('b', 'z', schema3['b']['z'].id),
+            ('b', 'x', schema1['b']['x'].id),
+            ('b', 'x', schema2['b']['x'].id),
+            ('b', 'y', schema1['b']['y'].id),
+            ('b', 'y', schema2['b']['y'].id),
+            ('c', schema1['c'].id),
+            ('c', schema2['c'].id),
+            ('y', schema3['y'].id),
+            ]
+        self.assertListEqual(expected, plan.keys())
+        for e in expected:
+            self.assertEqual(1, len(plan[e]))
+
+class SchemaToSubqueryTestCase(unittest.TestCase):
+    """
+    Verifies subquery exporting
+    """
+
+    layer = OCCAMS_DATASTORE_MODEL_FIXTURE
 
     def testExpectedMetadataColumns(self):
         session = self.layer['session']
@@ -138,7 +313,7 @@ class QueryTestCase(unittest.TestCase):
         session.add(schema)
         session.flush()
 
-        subquery = schemaToSubQuery(session, 'A', split=True)
+        subquery = schemaToSubquery(session, 'A')
         self.assertIn('entity_id', subquery.c)
         self.assertIn('entity_state', subquery.c)
         self.assertIn('entity_collect_date', subquery.c)
@@ -149,7 +324,7 @@ class QueryTestCase(unittest.TestCase):
         session.add(schema)
         session.flush()
 
-        subquery = schemaToSubQuery(session, 'A', split=True)
+        subquery = schemaToSubquery(session, 'A', split=True)
 
         self.assertEqual(0, session.query(subquery).count())
 
@@ -183,7 +358,7 @@ class QueryTestCase(unittest.TestCase):
         entity['a'] = u'barvalue'
         session.flush()
 
-        subquery = schemaToSubQuery(session, 'A', split=False)
+        subquery = schemaToSubquery(session, 'A', split=False)
 
         self.assertIn('a', subquery.c)
 
