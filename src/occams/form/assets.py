ce6180933a35eb2cf4f892ba8821a49b828317fd
Marco M <mam002@ucsd.edu>
2013-05-31 17:31:45 -0700

    OCCAMS-466 #comment Updated assets

--- src/occams/form/assets.py
+++ src/occams/form/assets.py
@@ -14,6 +14,11 @@ def config_assets(config):
     return config
 
 
+#
+# Third-Pary Libraries
+# These won't usually require filtering if the developer supplies them
+#
+
 bootstrap_css = Bundle('styles/bootstrap-2.3.2.min.css')
 bootstrap_js = Bundle('scripts/bootstrap-2.3.2.min.js')
 
@@ -22,30 +27,40 @@ jquery = Bundle('scripts/jquery-2.0.0.min.js')
 # We don't use the jquery-ui theme because of bootstrap
 jquery_ui_js = Bundle('scripts/jquery-ui-1.10.2.min.js')
 
-select2_css = Bundle('styles/select2-3.3.2.css', filters=('cssmin',))
+select2_css = Bundle('styles/select2-3.3.2.css', filters='cssmin')
 select2_js = Bundle('scripts/select2-3.3.2.min.js')
 
 deform_js = Bundle(
     resource_filename('deform', 'static/scripts/jquery.form-3.09.js'),
     resource_filename('deform', 'static/scripts/deform.js'),
-    filters=('jsmin',),
-    output='gen/deform-dependencies.js')
+    filters='jsmin')
 
+#
+# In-house Libraries
+#
 
-app_css = Bundle(
-   'styles/main.css',
-   filters=('cssmin',))
+#TODO modalize here
+
+#
+# Application-wide Assets
+# These should be avaialble globally throughout the application
+#
+
+app_css = Bundle('styles/app.*', filters='cssmin')
 app_js = Bundle(
     'scripts/modal.js',
     'scripts/app.js',
-    filters=('jsmin',))
+    filters='jsmin')
 
+#
+# Layout-specific Assets
+#
 
 default_css = Bundle(
     bootstrap_css,
     select2_css,
     app_css,
-    output='gen/default.css')
+    output='gen/default.%(version)s.css')
 
 default_js = Bundle(
     jquery,
@@ -54,5 +69,5 @@ default_js = Bundle(
     select2_js,
     deform_js,
     app_js,
-    output='gen/default.js')
+    output='gen/default.%(version)s.js')
 
