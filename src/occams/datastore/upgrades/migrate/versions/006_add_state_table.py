02b060417975b59c00d484ff937e8267ba1da750
Marco M <mam002@ucsd.edu>
2013-06-17 16:26:45 -0700

    OCCAMS-634 #time 2d #comment Fixed issues with upgrade step taking way too long #resolve

--- src/occams/datastore/upgrades/migrate/versions/006_add_state_table.py
+++ src/occams/datastore/upgrades/migrate/versions/006_add_state_table.py
@@ -61,122 +61,48 @@ def upgrade(migrate_engine):
     state_table = STATE_TABLE.tometadata(metadata)
     state_audit_table = STATE_AUDIT_TABLE.tometadata(metadata)
 
+    # Create the new state tables
+    state_table.create()
+    state_audit_table.create()
+
+    # Create new foreign key columns
+    Column('state_id', Integer).create(entity_table, index_name='ix_entity_state_id')
+    Column('state_id', Integer).create(entity_audit_table)
+
+    ForeignKeyConstraint(
+        [entity_table.c.state_id],
+        [state_table.c.id],
+        name='fk_entity_state_id'
+        ).create()
+
+    insert_query = state_table.insert().values(
+        name=bindparam('name'),
+        title=bindparam('title'),
+        create_user_id=select([user_table.c.id], user_table.c.key == BLAME_USER).as_scalar(),
+        modify_user_id=select([user_table.c.id], user_table.c.key == BLAME_USER).as_scalar(),
+        revision=1)
+
+    build_update = lambda t: t.update().values(
+        state_id=select([state_table.c.id], state_table.c.name == cast(t.c.state, String)).as_scalar())
+
     with migrate_engine.begin() as connection:
-        (blame_id,) = connection.execute(
-            select([user_table.c.id])
-            .where(user_table.c.key == BLAME_USER)
-            .limit(1)
-            ).fetchone()
-
-        # Create the new state tables
-        state_table.create(bind=connection)
-        state_audit_table.create(bind=connection)
-
-        # Auto-populate with previous enums
-        insert_query = (
-            state_table
-            .insert()
-            .values(
-                name=bindparam('name'),
-                title=bindparam('title'),
-                create_user_id=blame_id,
-                modify_user_id=blame_id,
-                revision=1))
-
         connection.execute(insert_query, [
             {'name': u'pending-entry', 'title': u'Pending Entry'},
+            {'name': u'in-progress', 'title': u'In Progress'},
             {'name': u'pending-review', 'title': u'Pending Review'},
-            {'name': u'pending-complete', 'title': u'Pending Complete'},
             {'name': u'pending-correction', 'title': u'Pending Correction'},
             {'name': u'complete', 'title': u'Complete'},
             # TODO: what to do with these:
             {'name': u'error', 'title': u'Error'},
-            {'name': u'inaccurate', 'title': u'Inaccurate'},
-            {'name': u'not-applicable', 'title': u'Not Applicable'},])
+            {'name': u'inaccurate', 'title': u'Inaccurate'}])
+        connection.execute(build_update(entity_table))
+        connection.execute(build_update(entity_audit_table))
 
-        # Create new foreign key columns
-        state_id_column = Column('state_id', Integer)
-        state_id_column.create(entity_table, index_name='ix_entity_state_id')
-        fk = ForeignKeyConstraint(
-            [state_id_column], [state_table.c.id], name='fk_entity_state_id')
-        fk.create(connection)
-
-        # Create audit columns
-        state_id_audit_column = Column('state_id', Integer)
-        state_id_audit_column.create(entity_audit_table, bind=connection)
-
-        result = connection.execute(select([state_table]))
-        state_id_map = dict([(r.name, r.id) for r in result])
-
-        # Migrate the data
-        for name, id in state_id_map.items():
-            connection.execute(
-                entity_table
-                .update()
-                .where(entity_table.c.state == name)
-                .values(state_id=id))
-
-        # Migrate the audit data
-        for name, id in state_id_map.items():
-            connection.execute(
-                entity_audit_table
-                .update()
-                .where(entity_audit_table.c.state == name)
-                .values(state_id=id))
-
-        # Drop previous columns
-        entity_table.c.state.drop(bind=connection)
-        entity_audit_table.c.state.drop(bind=connection)
+    # Drop previous columns
+    entity_table.c.state.drop()
+    entity_audit_table.c.state.drop()
 
 
 def downgrade(migrate_engine):
-    metadata = MetaData(migrate_engine)
-    entity_table = Table('entity', metadata, autoload=True)
-    entity_audit_table = Table('entity_audit', metadata, autoload=True)
-    user_table = Table('user', metadata, autoload=True)
-    state_table = Table(STATE_NAME, metadata, autoload=True)
-    state_audit_table = Table(STATE_AUDIT_NAME, metadata, autoload=True)
-
-    with migrate_engine.begin() as connection:
-        # build the states
-        result = connection.execute(select([state_table]))
-        states = dict([(r.id, r.name) for r in result])
-
-        # Re-add the enum
-        state_column = Column('state',
-            Enum(*sorted(states.keys()), name='entity_state'),
-            nullable=False,
-            server_default='pending-entry')
-        state_column.create(entity_table, bind=connection)
-
-        # re-add the audit enum
-        state_column = Column('state',
-            Enum(*storted(states.keys()), name='entity_state'),
-            nullable=False,
-            server_default='pending-entry')
-        state_column.create(entity_audit_table, bind=connection)
-
-        # Migrate the data
-        for id, name in states.items():
-            connection.execute(
-                entity_table
-                .update()
-                .where(entity_table.c.state_id == id)
-                .values(state=name))
-
-        # Migrate the audit data
-        for id, name in state.items():
-            connection.execute(
-                entity_audit_table
-                .update()
-                .where(entity_audit_table.c.state_id == id)
-                .values(state=name))
-
-        # Drop the foreign key columns
-        entity_table.c.state_id.drop(bind=connection)
-        entity_audit_table.c.state_id.drop(bind=connection)
-
-        # Drop previous columns
-        state_table.drop(bind=connection)
-        state_audit_table.drop(bind=connection)
+    raise Exception('Downgrading not supported')
 
