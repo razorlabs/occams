7527fe781bdd7f2bae4194418a5773d0ccc950d1
Marco M <mam002@ucsd.edu>
2013-06-17 13:25:05 -0700

    OCCAMS-638 #comment Upgrades 4/5 work now

--- src/occams/datastore/upgrades/migrate/versions/005_postgresql_upgrade.sql
+++ src/occams/datastore/upgrades/migrate/versions/005_postgresql_upgrade.sql
@@ -3,5 +3,44 @@
 -- to do a bit of hacking to get this to work properly.
 BEGIN;
 
+
+-- Prepare the live/audit tables for the upgrade
+
+
+ALTER TABLE "entity"
+  ALTER COLUMN "state" DROP DEFAULT,
+  ALTER COLUMN "state" DROP NOT NULL;
+
+ALTER TABLE "entity_audit"
+  ALTER COLUMN "state" DROP DEFAULT,
+  ALTER COLUMN "state" DROP NOT NULL;
+
+UPDATE "entity" SET "state" = NULL WHERE "state" = 'inline';
+UPDATE "entity_audit" SET "state" = NULL WHERE "state" = 'inline';
+
+
+-- Swap out enum types with the new one.
+
+
+ALTER TYPE "entity_state" RENAME TO "entity_state_old";
+
+CREATE TYPE "entity_state" AS ENUM (
+  'pending-entry',
+  'in-progress',
+  'pending-review',
+  'pending-correction',
+  'complete',
+  'error',
+  'inaccurate');
+
+ALTER TABLE "entity"
+  ALTER COLUMN "state" TYPE "entity_state" USING "state"::text::"entity_state";
+
+ALTER TABLE "entity_audit"
+  ALTER COLUMN "state" TYPE "entity_state" USING "state"::text::"entity_state";
+
+DROP TYPE "entity_state_old";
+
+
 COMMIT;
 
