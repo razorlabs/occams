0b877fc2f573253f5a1160800f45e8ff28286110
Marco Martinez <mam002@ucsd.edu>
2014-03-18 15:55:39 -0700

    Made partner tables id-agnostic

--- triggers/partner/partner.sql
+++ triggers/partner/partner.sql
@@ -6,6 +6,7 @@
 CREATE FOREIGN TABLE partner_ext (
     id                  SERIAL NOT NULL
 
+  , zid                 INTEGER NOT NULL
   , patient_id          INTEGER NOT NULL
   , enrolled_patient_id INTEGER
 
@@ -23,22 +24,41 @@ CREATE OR REPLACE FUNCTION partner_mirror() RETURNS TRIGGER AS $partner_mirror$
   BEGIN
     CASE TG_OP
       WHEN 'INSERT' THEN
-        INSERT INTO partner_ext SELECT NEW.*;
+        INSERT INTO partner_ext (
+            zid
+          , patient_id
+          , enrolled_patient_id
+          , create_date
+          , create_user_id
+          , modify_date
+          , modify_user_id
+          , revision
+        )
+        VALUES (
+            NEW.zid
+          , ext_patient_id(NEW.patient_id)
+          , ext_patient_id(NEW.enrolled_patient_id)
+          , NEW.create_date
+          , ext_user_id(NEW.create_user_id)
+          , NEW.modify_date
+          , ext_user_id(NEW.modify_user_id)
+          , NEW.revision
+        );
       WHEN 'DELETE' THEN
-        DELETE FROM partner_ext WHERE id = OLD.id;
+        DELETE FROM partner_ext WHERE zid = OLD.zid;
       WHEN 'TRUNCATE' THEN
         TRUNCATE partner_ext;
       WHEN 'UPDATE' THEN
         UPDATE partner_ext
-        SET id = NEW.id
-          , patient_id = NEW.patient_id
-          , enrolled_patient_id = NEW.enrolled_patient_id
+        SET zid = NEW.zid
+          , patient_id = ext_patient_id(NEW.patient_id)
+          , enrolled_patient_id = ext_patient_id(NEW.enrolled_patient_id)
           , create_date = NEW.create_date
           , create_user_id = ext_user_id(NEW.create_user_id)
           , modify_date = NEW.modify_date
           , modify_user_id = ext_user_id(NEW.modify_user_id)
           , revision = NEW.revision
-        WHERE id = OLD.id;
+        WHERE zid = OLD.zid;
     END CASE;
     RETURN NULL;
   END;
