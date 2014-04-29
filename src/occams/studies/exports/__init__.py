696db5440aa3dfc273bc4c4211ca9f10fe98f988
Marco Martinez <mam002@ucsd.edu>
2014-04-28 18:29:58 -0700

    Quote all columns

--- src/occams/studies/exports/__init__.py
+++ src/occams/studies/exports/__init__.py
@@ -55,7 +55,9 @@ def write_data(buffer, query):
              Note that the column names will be used as the header.
     """
     fieldnames = [d['name'] for d in query.column_descriptions]
-    writer = csv.DictWriter(buffer, fieldnames=fieldnames)
+    writer = csv.DictWriter(buffer,
+                            fieldnames=fieldnames,
+                            quoting=csv.QUOTE_ALL)
     writer.writeheader()
     writer.writerows([r._asdict() for r in query])
     buffer.flush()
@@ -69,7 +71,7 @@ def write_codebook(buffer, rows):
     buffer -- a file object which will be used to write data contents
     rows -- Code book rows. Seee `occams.studies.codebook`
     """
-    writer = csv.DictWriter(buffer, codebook.HEADER)
+    writer = csv.DictWriter(buffer, codebook.HEADER, quoting=csv.QUOTE_ALL)
     writer.writeheader()
 
     def choices2string(choices):
