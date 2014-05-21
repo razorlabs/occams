5fe236813a824747d0ebffa73ba017362288d0ba
Marco Martinez <mam002@ucsd.edu>
2014-05-21 11:12:28 -0700

    Only use roles necessary for launch

--- src/occams/studies/security.py
+++ src/occams/studies/security.py
@@ -26,24 +26,13 @@ def occams_groupfinder(identity, request):
     if 'groups' not in identity:
         log.warn('groups has not been set in the repoze identity!')
 
-    # TODO: move to externa bitcore auth module
-
-    mapping = {
-        'admins': 'administrator',
-        'nurses': 'nurse',
-        'statistician': 'analyst',
-        'pi': 'investigator'}
-
     def parse_group(name):
         parts = name.split('-')
         try:
             org, site, group = parts
         except ValueError:
             org, group = parts
-        if group in mapping:
-            return mapping[group]
-        else:
-            return name
+        return name
 
     return [parse_group(n) for n in identity['groups']]
 
@@ -60,25 +49,8 @@ class RootFactory(object):
 
     __acl__ = [
         (Allow, 'administrator', ALL_PERMISSIONS),
-        (Allow, 'investigator', (
-            'view',
-            'fia_view')),
-        (Allow, 'manager', (
-            'view',
-            'fia_view')),
-        (Allow, 'analyst', (
-            'view',
-            'fia_view')),
-        (Allow, 'nurse', (
-            'view'
-            'site_view',
-            'patient_add',  'patient_view',  'patient_edit',
-            'enrollment_add',  'enrollment_view',  'enrollment_edit',
-            'enrollment_delete',
-            'visit_add',  'visit_view',  'visit_edit',  'visit_delete')),
-        (Allow, 'assistant', ('view',)),
-        (Allow, 'student', ('view',)),
-        (Allow, Authenticated, 'view'),
+        (Allow, 'consumer', ('fia_view',)),
+        (Allow, Authenticated, ('view',)),
         ]
 
     def __init__(self, request):
