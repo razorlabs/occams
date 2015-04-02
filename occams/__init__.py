414c5fef6a9295dc7f673ef6a829c17bddc03f45
Marco Martinez <mam002@ucsd.edu>
2015-04-02 16:49:01 -0700

    Re-use pyramid_rewrite...

--- occams/__init__.py
+++ occams/__init__.py
@@ -62,6 +62,8 @@ def main(global_config, **settings):
     # Built-in plugins
     config.include('pyramid_chameleon')
     config.include('pyramid_redis_sessions')
+    config.include('pyramid_rewrite')
+    config.add_rewrite_rule(r'/(?P<path>.*)/', r'/%(path)s')
     config.include('pyramid_tm')
     config.include('pyramid_webassets')
     config.commit()
