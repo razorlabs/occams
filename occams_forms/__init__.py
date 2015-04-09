1fad0e5b919149fa7847b1badcfc7d5732495170
Marco Martinez <mam002@ucsd.edu>
2015-04-09 15:27:56 -0700

    Use new app listing method

--- occams_forms/__init__.py
+++ occams_forms/__init__.py
@@ -8,7 +8,7 @@ from occams import Session
 
 wtforms_json.init()  # monkey-patch wtforms to accept JSON data
 
-log = logging.getLogger(__name__)
+log = logging.getLogger('occams').getChild(__name__)
 
 _ = TranslationStringFactory(__name__)
 
@@ -18,6 +18,15 @@ __version__ = pkg_resources.require(__name__)[0].version
 
 
 def includeme(config):
+
+    config.registry.settings['occams.apps']['occams_forms'] = {
+        'name': 'forms',
+        'title': _(u'Forms'),
+        'package': 'occams_forms',
+        'route': 'forms.main',
+        'version': __version__
+    }
+
     config.include('.assets')
     config.include('.routes')
     config.scan()
