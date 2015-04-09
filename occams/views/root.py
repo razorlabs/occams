abfb3dbe277c06f131b12f0566fc0528fcf4f886
Marco Martinez <mam002@ucsd.edu>
2015-04-09 15:26:02 -0700

    All apps to register themselves in the app listing

--- occams/views/root.py
+++ occams/views/root.py
@@ -1,5 +1,3 @@
-
-from pyramid.path import DottedNameResolver
 from pyramid.view import view_config
 
 
@@ -11,23 +9,4 @@ def root(context, request):
     """
     Renders all available applciations
     """
-
-    resolver = DottedNameResolver()
-
-    apps = []
-
-    for dotted in request.registry.settings['occams.apps']:
-        module = resolver.resolve(dotted)
-        if hasattr(module, '__prefix__'):
-            apps.append({
-                'name': dotted,
-                'title': module.__title__,
-                # TODO: maybe use a default view instead?
-                'url': module.__prefix__
-            })
-
-    apps = sorted(apps, key=lambda v: v['title'])
-
-    return {
-        'apps': apps,
-        }
+    return {}
