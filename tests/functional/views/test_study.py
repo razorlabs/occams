e6e064036c8c2138127d6b58dfe608a27a1ca2af
Marco Martinez <mam002@ucsd.edu>
2015-04-23 19:40:52 -0700

    Figured out how to add data and make it available to the test request

--- tests/functional/views/test_study.py
+++ tests/functional/views/test_study.py
@@ -1,6 +1,6 @@
 from ddt import ddt, data
 
-from tests import FunctionalFixture
+from tests import FunctionalFixture, USERID
 
 
 @ddt
@@ -8,11 +8,24 @@ class TestPermissionsStudyList(FunctionalFixture):
 
     url = '/studies'
 
+    def setUp(self):
+        super(TestPermissionsStudyList, self).setUp()
+
+        import transaction
+        from occams import Session
+        from occams_datastore import models as datastore
+
+        # Any view-dependent data goes here
+        # Webtests will use a different scope for its transaction
+        with transaction.manager:
+            Session.add(datastore.User(key=USERID))
+
     @data('administrator', 'investigator', 'statistician', 'researcher',
           'nurse', 'assistant', 'student', None)
     def test_allowed(self, group):
-        environ = self.make_environ(groups=[group])
-        self.app.get(self.url, extra_environ=environ, status=200)
+        environ = self.make_environ(userid=USERID, groups=[group])
+        response = self.app.get(self.url, extra_environ=environ)
+        self.assertEquals(200, response.status_code)
 
     def test_not_authenticated(self):
         self.app.get(self.url, status=403)
