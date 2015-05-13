eb871339319d8718767c1a58af81303457971935
Jim Krooskos <jkrooskos@ucsd.edu>
2015-05-13 14:43:16 -0700

    Add debug authorization to settings fixture

--- tests/__init__.py
+++ tests/__init__.py
@@ -131,6 +131,7 @@ class FunctionalFixture(unittest.TestCase):
 
             # Enable regular error messages so we can see useful traceback
             'debugtoolbar.enabled': True,
+            'pyramid.debug_authorization': True,
 
             'webassets.debug': True,
             'webassets.auto_build': False,
