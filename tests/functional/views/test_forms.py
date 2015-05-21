4cda47c58f75ca4ad1c6940bbbe159d0e376898e
Jim Krooskos <jkrooskos@ucsd.edu>
2015-05-21 16:24:48 -0700

    Add setUp test method

--- tests/functional/views/test_forms.py
+++ tests/functional/views/test_forms.py
@@ -5,4 +5,29 @@ from tests import FunctionalFixture, USERID
 
 @ddt
 class TestPermissionForms(FunctionalFixture):
-    pass
+    def setUp(self):
+        super(TestPermissionForms, self).setUp()
+
+        from datetime import date
+
+        import transaction
+        from occams import Session
+        from occams_studies import models as forms
+        from occams_datastore import models as datastore
+
+        # Any view-dependent data goes here
+        # Webtests will use a different scope for its transaction
+        with transaction.manager:
+            user = datastore.User(key=USERID)
+            Session.info['blame'] = user
+            Session.add(user)
+            Session.flush()
+
+            form = datastore.Schema(
+                name=u'test_schema',
+                title=u'test_title',
+                publish_date=date(2015, 1, 1)
+            )
+
+            Session.add(form)
+            Session.flush()
