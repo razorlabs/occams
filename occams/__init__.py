c9a97ea61cb99daad9d889d644225685164d957b
Jim Krooskos <jkrooskos@ucsd.edu>
2016-02-05 16:26:04 -0800

    Add config for production to settings

--- occams/__init__.py
+++ occams/__init__.py
@@ -12,6 +12,7 @@ from pyramid.i18n import TranslationStringFactory
 from pyramid.renderers import JSON
 from pyramid.settings import aslist
 from pyramid_who.whov2 import WhoV2AuthenticationPolicy
+from pyramid.settings import asbool
 import wtforms_json; wtforms_json.init()
 
 __version__ = pkg_resources.require(__name__)[0].version
@@ -51,6 +52,11 @@ def main(global_config, **settings):
     settings['occams.apps'] = dict.fromkeys(apps)
 
     settings.update(piwik_from_config(settings))
+
+    # determine if deployment is production
+    production_deployment = asbool(settings.get(
+        'production_deployment', 'false'))
+    settings['production_deployment'] = production_deployment
 
     config = Configurator(
         settings=settings,
