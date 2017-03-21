d283a65a61c1a7f604d2163ac3da518385b7c7c5
Marco Martinez <m-martinez@users.noreply.github.com>
2017-03-21 11:45:42 -0700

    Use version range wildcard on patch versions to produce reliable builds

--- setup.py
+++ setup.py
@@ -1,91 +1,23 @@
+import re
 import os
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop as _develop
-import sys
+
+
+def yield_packages(path):
+    """Simple parser for requirements.txt files"""
+    with open(path) as requirements:
+        for line in requirements:
+            if re.match('^[a-zA-Z]', line):
+                yield line.strip()
+
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(HERE, 'README.rst')).read()
 CHANGES = open(os.path.join(HERE, 'CHANGES.rst')).read()
 
-
-REQUIRES = [
-    'alembic',                          # Database table upgrades
-    'babel',                            # i18n
-    'celery[redis]>=3.1,<3.1.99',       # Asynchronous queue API
-    'cssmin',                           # CSS asset compression
-    'gevent',
-    'gunicorn==19.3',                   # WSGI server
-                                        # Use 19.3 as 19.4 has some issues
-    'humanize',                         # human readable measurements
-    'jsmin',                            # JS asset compression
-    'lingua',                           # i18n
-    'python-dateutil',                  # Date parsing
-    'python-slugify',                   # path-friendly filenames
-    'pyramid>=1.7',                     # Framework
-    'pyramid_chameleon',                # Templating
-    'pyramid_exclog',                   # Logging for production
-    'pyramid_tm>=1.1.1,<1.99.99',         # Centralized transations
-    'pyramid_redis_sessions',           # HTTP session with redis backend
-    'pyramid_redis',
-    'pyramid_webassets',                # Asset management (ala grunt)
-    'pyramid_who',                      # User authentication
-    'repoze.who>=2.3.0',
-    'rutter',                           # Virtual path mounting
-    'six',                              # Py 2 & 3 compatibilty
-    'SQLAlchemy>=1.0.17,<1.0.99',        # Database ORM
-    'tabulate',                         # ASCII tables for CLI pretty-print
-    'wtforms>=2.0.0',
-    'wtforms-json',
-    'wtforms-components',
-    'zope.sqlalchemy',                  # Connects sqlalchemy to pyramid_tm
-
-    'occams_datastore',                 # It's an utility, not an app
-]
-
-EXTRAS = {
-
-    'apps': [                           # Default applications
-        'occams_accounts',
-        'occams_studies',
-        'occams_forms'
-        'occams_lims'
-        'occams_reports',
-    ],
-
-    'docs': [                           # Documentation building
-        'sphinx',
-        'sphinx-autobuild'
-    ],
-
-    'ldap': ['who_ldap>=3.2.2'],        # LDAP authorization
-
-    'sqlite': [],
-
-    'postgresql': [
-        'psycopg2',
-        'psycogreen'
-    ],
-
-    'test': [
-        'pyramid_debugtoolbar',
-        'pytest',
-        'pytest-cov',
-        'factory_boy>=2.8.1',
-        'WebTest',
-        'beautifulsoup4',
-        'mock',
-        'who_dev>=0.0.2',
-    ],
-}
-
-
-if sys.version_info < (2, 7):
-    REQUIRES.extend(['argparse', 'ordereddict'])
-    EXTRAS['test'].extend(['unittest2'])
-
-
-if sys.version_info < (3, 0):
-    REQUIRES.extend(['unicodecsv'])
+REQUIRES = list(yield_packages(os.path.join(HERE, 'requirements.txt')))
+DEVELOP = list(yield_packages(os.path.join(HERE, 'requirements-develop.txt')))
 
 
 def get_version():
@@ -156,9 +88,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=REQUIRES,
-    extras_require=EXTRAS,
-    tests_require=EXTRAS['test'],
-    test_suite='nose.collector',
+    extras_require={'test': DEVELOP},
+    tests_require=DEVELOP,
     cmdclass={'develop': _custom_develop},
     entry_points="""\
     [paste.app_factory]
